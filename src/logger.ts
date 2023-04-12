2310e93f59d286819ae297ee28a3a4371906fdf0
Serge Kovbasiuk <isserge@protonmail.com>
2023-04-12 20:26:09 +0200

    update logger

--- src/logger.ts
+++ src/logger.ts
@@ -1,14 +1,18 @@
 import { pino } from "pino";
 
+const logLevel = process.env.LOG_LEVEL || 'info';
+const logColorize = process.env.LOG_COLORIZE === 'true' || true;
+const logTranslateTime = process.env.LOG_TRANSLATE_TIME || 'yyyy-mm-dd HH:MM:ss';
+const logIgnore = process.env.LOG_IGNORE || 'hostname,pid';
+
 export const logger = pino({
+  level: logLevel,
   transport: {
     target: 'pino-pretty',
     options: {
-      colorize: true,
-      translateTime: 'yyyy-mm-dd HH:MM:ss',
-      ignore: 'hostname,pid',
-    }
+      colorize: logColorize,
+      translateTime: logTranslateTime,
+      ignore: logIgnore,
+    },
   },
-})
-
-logger.level = process.env.DEBUG ? 'debug' : 'info';
+});
