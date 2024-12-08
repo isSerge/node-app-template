762e8f108c987024fcaf051539db9b807599e048
Serge Kovbasiuk <isserge@protonmail.com>
2024-12-08 20:17:21 +0700

    chore: update project to target ESM

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { logger } from './logger';
+import { logger } from './logger.js';
 
 export function main() {
   logger.info('Hello World');
--- src/test/test.test.ts
+++ src/test/test.test.ts
@@ -1,6 +1,6 @@
 import { it } from 'node:test';
 import * as assert from 'node:assert/strict';
-import { main } from '../index';
+import { main } from '../index.js';
 
 it('dummy test', () => {
   assert.doesNotThrow(main);
